16dbaba92b4003aaed322e268f69071d607093b3
ComoRoi <rsiwin@naver.com>
2022-05-19 13:58:03 +0900

    v2.3.0 신분증 UI 개편에 따라 수정반영

--- initial_ui_check.py
+++ initial_ui_check.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     try :
         d.app_start(comConSet['app_package'])
 
-        pid = d.app_wait(comConSet['app_package'], timeout=20.0) #실행대기
+        pid = d.app_wait(comConSet['app_package'], timeout=3.0) #실행대기
         if not pid:
             print(currentTime() + targetService + "com.example.android is not running")
             assert False, f"{comConSet['app_package']}가 정상적으로 수행되지 않았습니다."
@@ -85,23 +85,27 @@ if __name__ == '__main__':
     try :
         #  고려대 학생증 발급
         sleep(1) #대기
-        d.swipe(0.691, 0.236, 0.075, 0.236, 0.1)
-        sleep(0.3)
-        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/list_id_credential"]/android.view.ViewGroup[2]/androidx.cardview.widget.CardView[1]/android.view.ViewGroup[1]/android.widget.ImageView[1]').click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_issuing_credentials").click()
         sleep(0.3) #대기
-        d(resourceId="com.sktelecom.myinitial:id/contentPanel").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="대학 신분증").click()
         sleep(0.3) #대기
-        d(resourceId="com.sktelecom.myinitial:id/iconImg").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="고려대학교").click()
         sleep(0.3) #대기
-        d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="연결하기").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="대학 신분증").click()
         sleep(0.3) #대기
-        d(resourceId="com.sktelecom.myinitial:id/edit_university_id", text="학번 또는 교직원 번호를 입력해주세요").set_text(priConSet['universityId'])
-        d.swipe_ext("up", 0.6)
+        d(resourceId="com.sktelecom.myinitial:id/edit_university_id", text="학번 또는 교직원 번호 정보를 입력해주세요").set_text(priConSet['universityId'])
+        sleep(0.3) #대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_sub_title", text="고려대학교").click()
         sleep(0.3) #대기
         d(text="약관동의 및 증명서 제출", className="android.widget.Button").click()
         sleep(0.3) #대기
-        d(text="저장하기").click()
-
+        d(text="보관함에서 확인하기").click()
+        sleep(0.3)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/img_qr", className="android.widget.ImageView").click()
+        sleep(0.3)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/btn_close", className="android.widget.ImageButton").click()
+        sleep(0.3)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/btn_close", className="android.widget.ImageButton").click()
     except :
         errCode = 2010
         err.errorReport(targetService, errCode, errConSet[errCode])
@@ -109,19 +113,19 @@ if __name__ == '__main__':
     try :
         #  고려대 학생증 삭제
         sleep(1) #대기
-        d(resourceId="com.sktelecom.myinitial:id/titleRightSetBtn").click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_edit_id_credential").click()
         sleep(0.2) #대기
-        d(text="삭제").click()
+        d(className="android.widget.TextView", text="삭제").click()
         sleep(0.2) #대기
         d(resourceId="com.sktelecom.myinitial:id/flow_end_btn").click()
         sleep(0.2) #대기
-        d(text="예").click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_positive",text="예").click()
         sleep(0.2) #대기
-        d(text="저장").click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_save", text="저장").click()
         sleep(0.2) #대기
-        d(text="예").click()
+        d(className="android.widget.Button", text="예").click()
         sleep(0.2) #대기
-        d(className="android.widget.ImageButton").click()
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/btn_back"]').click()
 
     except :
         errCode = 2011
@@ -130,31 +134,23 @@ if __name__ == '__main__':
     try :
         # 삼성 수리비 영수증 및 내역서
         sleep(0.5) #대기
-        d.swipe(0.351, 0.665, 0.351, 0.105, 0.1)
+        d(resourceId="com.sktelecom.myinitial:id/btn_issuing_credentials").click()
+        sleep(1)  # 대기
+        d(className="android.widget.TextView", text="증명서").click()
+        sleep(1)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="수리비 영수증 및 내역서").click()
         sleep(1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/nameTxt", text = "수리비 영수증 \n및 내역서").click()
-        d(resourceId="com.sktelecom.myinitial:id/txt_shortcut_name", text = "수리비 영수증 및 내역서").click()
-        sleep(1) #대기
-        d(resourceId="com.sktelecom.myinitial:id/labelTxt", text="삼성서비스센터").click()
-        sleep(1) #대기
-        d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="연결하기").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="삼성서비스센터").click()
         sleep(1) #대기
         d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="약관동의 및 증명서 제출").click()
         sleep(1) #대기
         d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="확인").click()
         sleep(1) #대기
-        d(text="확인").click()
-        #d(resourceId="com.sktelecom.myinitial:id/closeBtn").click()
-        # d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="확인").click()
-        #sleep(0.1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/selectChk").click()
-        #sleep(0.1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/closeBtn").click()
-        #sleep(0.1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/selectChk", className="android.widget.CheckBox").click()
-        #d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="발급 신청하기").click()
-        #d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text = "저장하기").click()
-        #d(resourceId="com.sktelecom.myinitial:id/closeBtn").click()
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_card"]/android.view.ViewGroup[1]/android.view.View[2]').click()
+        sleep(1)  # 대기
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/btn_back"]').click()
+        sleep(1)  # 대기
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_end_button"]').click()
 
     except :
         errCode = 2020
@@ -163,19 +159,21 @@ if __name__ == '__main__':
     try :
         # LG 수리비 영수증 및 내역서
         sleep(0.5) #대기
-        d.swipe(0.351, 0.665, 0.351, 0.105)
+        d(resourceId="com.sktelecom.myinitial:id/btn_issuing_credentials").click()
         sleep(1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/nameTxt", text = "수리비 영수증 \n및 내역서").click()
-        d(resourceId="com.sktelecom.myinitial:id/txt_shortcut_name", text = "수리비 영수증 및 내역서").click()
-        sleep(1) #대기
-        d(resourceId="com.sktelecom.myinitial:id/labelTxt", text="LG전자").click()
-        sleep(1) #대기
-        d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="연결하기").click()
-        sleep(1) #대기
+        d(className="android.widget.TextView", text="증명서").click()
+        sleep(1)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="수리비 영수증 및 내역서").click()
+        sleep(1)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="LG전자").click()
+        sleep(1)  # 대기
         d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="약관동의 및 증명서 제출").click()
-        sleep(1) #대기
-        #d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="확인").click()
-        d(text="확인").click()
+        sleep(1)  # 대기
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_card"]/android.view.ViewGroup[1]/android.view.View[2]').click()
+        sleep(1)  # 대기
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/btn_back"]').click()
+        sleep(1)  # 대기
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_end_button"]').click()
 
     except :
         errCode = 2021
@@ -184,19 +182,19 @@ if __name__ == '__main__':
     try :
         ## 성적증명서 발급준비
         sleep(0.5) #대기
-        ## 출입권한증명 PS&M
-        d(text="교육").click()
-        d(resourceId="com.sktelecom.myinitial:id/txt_shortcut_name", text="대학교 성적증명서").click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_issuing_credentials").click()
+        sleep(1)  # 대기
+        d(className="android.widget.TextView", text="증명서").click()
+        sleep(0.5)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="대학교 성적증명서").click()
         sleep(0.2)
         # 대학 검색
-        d(text="대학명을 검색해주세요").set_text("백석대")
+        d(text="찾으시는 대학명을 입력해주세요.").set_text("백석대학교")
         sleep(0.2) #대기
-        d(resourceId="com.sktelecom.myinitial:id/searchBtn").click()
-        sleep(0.2) #대기
-        d(resourceId="com.sktelecom.myinitial:id/labelTxt", text="백석대학교").click()
-        sleep(0.2) #대기
-        d(text="연결하기").click()
-        sleep(0.2) #대기
+        d(resourceId="com.sktelecom.myinitial:id/btn_search_query").click()
+        sleep(1)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="백석대학교").click()
+        sleep(0.5) #대기
         d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text="약관동의 및 증명서 제출").click()
 
         # 대학 웹뷰 확인
@@ -204,6 +202,8 @@ if __name__ == '__main__':
         d(resourceId="com.sktelecom.myinitial:id/btn_gov_close").click()
         sleep(0.2)
         d(text="확인").click()
+        sleep(0.2)
+        d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_end_button"]').click()
 
     except :
         errCode = 2030
@@ -211,27 +211,27 @@ if __name__ == '__main__':
 
     try :
         ## 출입권한증명 PS&M
-        d(text = "신분·출입").click()
+        sleep(0.5)  # 대기
+        d(resourceId="com.sktelecom.myinitial:id/btn_issuing_credentials").click()
+        sleep(1)  # 대기
+        d(className="android.widget.TextView", text="증명서").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text="출입권한증명").click()
         sleep(0.2) #대기
-        d(resourceId="com.sktelecom.myinitial:id/txt_shortcut_name", text = "출입권한증명").click()
-        sleep(0.2) #대기
-        d(resourceId="com.sktelecom.myinitial:id/labelTxt", text = "PS&M").click()
-        sleep(0.2) #대기
-        d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text = "연결하기").click()
+        d(resourceId="com.sktelecom.myinitial:id/txt_name", text = "PS&M").click()
         sleep(0.2) #대기
         d(resourceId="com.sktelecom.myinitial:id/confirmBtn", text = "약관동의 및 증명서 제출").click()
         sleep(0.2) #대기
-        #d.xpath('//android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]').click() **********
         d.xpath('//*[@resource-id="com.sktelecom.myinitial:id/lyt_card"]/android.view.ViewGroup[1]/android.view.View[2]').click()
+        sleep(0.2) #대기
+        d(resourceId="com.sktelecom.myinitial:id/lyt_end_button").click()
+
+
     except :
         errCode = 2040
         err.errorReport(targetService, errCode, errConSet[errCode])
 
-
     try :
         #행정안정부 전자문서지갑
-        sleep(1) #대기
-        d.swipe(0.151, 0.305, 0.351, 0.665, 0.1)
         sleep(1) #대기
         d(resourceId="com.sktelecom.myinitial:id/btn_navigate_to_go_certification_apply", text="증명서 신청").click()
         sleep(1.5) #대기
@@ -245,7 +245,7 @@ if __name__ == '__main__':
         sleep(1.5) #대기
         d(resourceId="com.sktelecom.myinitial:id/lyt_end_button").click()
         sleep(1.5) #대기
-        d(resourceId="com.sktelecom.myinitial:id/btn_navigate_to_go_certification_wallet", text="정부 전자문서지갑").click()
+        d(resourceId="com.sktelecom.myinitial:id/btn_navigate_to_go_certification_wallet", text="전자문서지갑").click()
         sleep(1.5) #대기
         d(resourceId="com.sktelecom.myinitial:id/lyt_end_button").click()
         sleep(1.5) #대기
@@ -254,15 +254,6 @@ if __name__ == '__main__':
         d(text="주소 복사하기").click()
         sleep(1.5) #대기
         d(resourceId="com.sktelecom.myinitial:id/lyt_end_button").click()
-        sleep(1.5) #대기
-        d(resourceId="com.sktelecom.myinitial:id/txt_issuable_certificate_name", text = "건강보험자격득실확인서").click()
-        sleep(1.5) #대기
-        d(text="발급 신청하기").click()
-        sleep(1.5) #대기
-        d.xpath('//*[@resource-id="app"]/android.view.View[2]/android.view.View[1]/android.view.View[1]').click()
-        d(text="뒤로가기").click()
-        sleep(1.5) #대기
-        d(text="예").click()
 
     except :
         errCode = 2000
@@ -287,3 +278,12 @@ else :
     print (currentTime() + targetService + "Test Result   : Success !!")
     err.normalReport()
     #bot.post_message(botChannel, targetService + "initial service is OK.")
+
+
+
+
+
+
+
+
+
